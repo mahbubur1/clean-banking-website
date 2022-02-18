ee079499ee8b59b8cd52cacbeef66f810adff1f6
mahbubur1 <mdmahbuborr462@gail.com>
2022-02-18 10:14:53 +0600

    some comment out in clean.js file & add event listener in  withdraw button & withdraw button clicked withdraw amount set

--- js/clean.js
+++ js/clean.js
@@ -1,3 +1,4 @@
+// get user input from input field 
 function getInputValue (inputId){
     const inputField = document.getElementById(inputId);
     const inputText =inputField.value;
@@ -5,7 +6,7 @@ function getInputValue (inputId){
     inputField.value = '';
     return value;
 }
-
+ // set total amount in total field  
 function updateTotal (fieldId,amount){
     const totalTag = document.getElementById(fieldId);
     const totalTagText = totalTag.innerText;
@@ -13,8 +14,13 @@ function updateTotal (fieldId,amount){
     const currentTotal = perviosTotal + amount;
     totalTag.innerText = currentTotal;
 }
-
+ // deposit button click event listener added 
 document.getElementById('deposit-button').addEventListener('click',function(){
    const depositInput =  getInputValue ('deposit-input');
-   const depositTotal = updateTotal('deposit-total',depositInput)
+             updateTotal('deposit-total',depositInput)
 })
+ // withdraw button click event listener added 
+ document.getElementById('withdraw-button').addEventListener('click',function(){
+     const withdrawInput = getInputValue('withdraw-input');
+                updateTotal('withdraw-total',withdrawInput);
+ })
