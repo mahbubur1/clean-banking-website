539416a533ccd18b870b365edc707c01cdf36500
mahbubur1 <mdmahbuborr462@gail.com>
2022-02-18 10:30:55 +0600

    update balance

--- js/clean.js
+++ js/clean.js
@@ -14,10 +14,19 @@ function updateTotal (fieldId,amount){
     const currentTotal = perviosTotal + amount;
     totalTag.innerText = currentTotal;
 }
+ // update balance set 
+ function updateBalance (amount){
+     const balanceTag = document.getElementById('total-amount');
+     const balanceTagInText = balanceTag.innerText;
+     const perviosBalance = parseFloat(balanceTagInText);
+     const currentBalance = perviosBalance + amount;
+     balanceTag.innerText = currentBalance;
+ }
  // deposit button click event listener added 
 document.getElementById('deposit-button').addEventListener('click',function(){
    const depositInput =  getInputValue ('deposit-input');
-             updateTotal('deposit-total',depositInput)
+             updateTotal('deposit-total',depositInput);
+             updateBalance(depositInput);
 })
  // withdraw button click event listener added 
  document.getElementById('withdraw-button').addEventListener('click',function(){
